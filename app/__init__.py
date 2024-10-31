b9aba8cec1f54d704711ec65ea9aa95ce4cde479
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-31 14:49:28 -0400

    updated config

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from .routes.planet_routes import planet_bp
 from .db import db, migrate
 from .models import planet
+import os
 
-def create_app(test_config=None):
+def create_app(config=None):
     app = Flask(__name__)
 
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('SQLALCHEMY_DATABASE_URI')
 
+    if config:
+        app.config.update(config)
+    
     db.init_app(app)
     migrate.init_app(app, db)
 
