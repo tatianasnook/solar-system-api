f98a7e1e8afd19f2477d8e8cbddc69442992d872
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-11-01 11:44:20 -0400

    added additional tests

--- tests/test_planet_routes.py
+++ tests/test_planet_routes.py
@@ -10,12 +10,14 @@ def test_get_one_planet_succeeds(client, two_saved_planets):
         "num_of_moons": 0
     }
 
+
 def test_get_one_planet_not_found_in_empty_db(client):
     response = client.get("planets/1")
     response_body = response.get_json()
 
     assert response.status_code == 404
     assert response_body == {"message": f"planet 1 not found"}
+
 
 def test_get_one_planet_not_found_with_planets_in_db(client, two_saved_planets):
     response = client.get("planets/3")
@@ -24,12 +26,14 @@ def test_get_one_planet_not_found_with_planets_in_db(client, two_saved_planets):
     assert response.status_code == 404
     assert response_body == {"message": f"planet 3 not found"}
 
+
 def test_get_all_planets_with_no_records(client):
     response = client.get("/planets")
     response_body = response.get_json()
 
     assert response.status_code == 200
     assert response_body == []
+
 
 def test_get_all_planets_with_planets_in_db(client, two_saved_planets):
     response = client.get("planets")
@@ -49,6 +53,7 @@ def test_get_all_planets_with_planets_in_db(client, two_saved_planets):
         "num_of_moons": 0
     }]
 
+
 def test_create_one_planet_in_empty_db(client):
     response = client.post("/planets", json={
         "name": "Pluto",
@@ -65,6 +70,7 @@ def test_create_one_planet_in_empty_db(client):
         "num_of_moons": 5
     }
 
+
 def test_create_one_planet_with_planets_already_in_db(client, two_saved_planets):
     response = client.post("/planets", json={
         "name": "Pluto",
@@ -80,3 +86,19 @@ def test_create_one_planet_with_planets_already_in_db(client, two_saved_planets)
         "description": "dwarf planet",
         "num_of_moons": 5
     }
+
+
+def test_update_planet_succeeds(client, two_saved_planets):
+    response = client.put("planets/1", json={
+        "name": "Mercury",
+        "description": "first planet in the solar system",
+        "num_of_moons": 0
+    })
+    assert response.status_code == 204
+    assert response.content_length is None
+
+
+def test_delete_one_planet_with_planets_already_in_db(client, two_saved_planets):
+    response = client.delete("planets/1")
+    assert response.status_code == 204
+    assert response.content_length is None
