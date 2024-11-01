9e5727e959b4bf3c65c1183923b6dc718eaf4b3c
Jen Nguyen <26128269+ItsJenOClock@users.noreply.github.com>
2024-10-31 22:57:09 -0400

    update tests

--- tests/test_planet_routes.py
+++ tests/test_planet_routes.py
@@ -1,11 +1,4 @@
-def test_get_all_planets_with_no_records(client):
-    response = client.get("/planets")
-    response_body = response.get_json()
-
-    assert response.status_code == 200
-    assert response_body == []
-
-def test_get_one_planet_success(client, two_saved_planets):
+def test_get_one_planet_succeeds(client, two_saved_planets):
     response = client.get("planets/1")
     response_body = response.get_json()
 
@@ -17,14 +10,28 @@ def test_get_one_planet_success(client, two_saved_planets):
         "num_of_moons": 0
     }
 
-def test_get_one_planet_failure(client):
+def test_get_one_planet_not_found_in_empty_db(client):
     response = client.get("planets/1")
     response_body = response.get_json()
 
     assert response.status_code == 404
     assert response_body == {"message": f"planet 1 not found"}
 
-def test_get_all_planets_success(client, two_saved_planets):
+def test_get_one_planet_not_found_with_planets_in_db(client, two_saved_planets):
+    response = client.get("planets/3")
+    response_body = response.get_json()
+
+    assert response.status_code == 404
+    assert response_body == {"message": f"planet 3 not found"}
+
+def test_get_all_planets_with_no_records(client):
+    response = client.get("/planets")
+    response_body = response.get_json()
+
+    assert response.status_code == 200
+    assert response_body == []
+
+def test_get_all_planets_with_planets_in_db(client, two_saved_planets):
     response = client.get("planets")
     response_body = response.get_json()
 
@@ -42,7 +49,7 @@ def test_get_all_planets_success(client, two_saved_planets):
         "num_of_moons": 0
     }]
 
-def test_create_one_planet(client):
+def test_create_one_planet_in_empty_db(client):
     response = client.post("/planets", json={
         "name": "Pluto",
         "description": "dwarf planet",
@@ -53,6 +60,22 @@ def test_create_one_planet(client):
     assert response.status_code == 201
     assert response_body == {
         "id": 1,
+        "name": "Pluto",
+        "description": "dwarf planet",
+        "num_of_moons": 5
+    }
+
+def test_create_one_planet_with_planets_already_in_db(client, two_saved_planets):
+    response = client.post("/planets", json={
+        "name": "Pluto",
+        "description": "dwarf planet",
+        "num_of_moons": 5
+    })
+    response_body = response.get_json()
+
+    assert response.status_code == 201
+    assert response_body == {
+        "id": 3,
         "name": "Pluto",
         "description": "dwarf planet",
         "num_of_moons": 5
